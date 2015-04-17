JNRowe/vim-configs

commit c0eb787b566ac733fc4f6b6aa2f964d35c8840f3

Don't restore position in git buffers.

vimrc
@@ -170,12 +170,7 @@ if has("autocmd")
 
     autocmd FileType text setlocal textwidth=72
 
-    " Jump to the last known cursor position if possible, taken from the
-    " wiki
-    autocmd BufReadPost *
-        \ if line("'\"") > 0 && line("'\"") <= line("$") |
-        \   exe "normal g`\"" |
-        \ endif
+    autocmd BufReadPost * if &ft =~ '^git' | exec "normal gg" | endif
 
     autocmd BufNewFile,BufRead /tmp/*-sup.*
         \ set ft=mail |

commit 7a47a99fff85ed547714dcc07e2a405ee2b34638

Restore last-used position restoring
This fixes a regression from c0eb787b566ac733fc4f6b6aa2f964d35c8840f3, where the
system vim doesn't set this by default(which to be fair is all /reasonable/
distros).

vimrc
@@ -166,9 +166,15 @@ if has("autocmd")
     " Automatically chmod +x shell scripts
     autocmd BufWritePost *.sh silent !chmod +x %
 
-    " Don't restore saved position for git buffers.
-    " This is most useful in commit messages.
-    autocmd BufReadPost * if &ft =~# '^git' | execute "normal gg" | endif
+    " Jump to the last known cursor position if possible.
+    " Note: Don't restore saved position for git buffers as it tends not to be
+    " useful.
+    autocmd BufReadPost *
+        \ if &filetype =~# '^git' |
+        \   execute "normal gg" |
+        \ elseif line("'\"") > 0 && line("'\"") <= line("$") |
+        \   execute "normal g`\"" |
+        \ endif
 
     autocmd BufNewFile,BufRead *.rb set sw=2
 